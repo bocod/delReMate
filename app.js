480c90217e55e9ab3252669003165878535d876d
Agustin Boccio <aboccio@hotmail.com>
2021-11-08 18:55:13 -0300

    app.js, productCart.html styles and site theme

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(process.env.PORT || 3000);
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'))
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productCart.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+});
